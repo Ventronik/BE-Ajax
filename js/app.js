9bca01f4e9478dd98a4cf3eb62e82c3302a793f7
Dustin Ruskell <druskell@gmail.com>
2018-05-14 19:04:14 -0700

    fixed port problem

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ app.use(function(err, req, res, next){
 // Starting Server
 //////////////////////////////////////////////////////////////////////////////
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, function(){
   console.log(`Listening on port ${port}`)
